c78e4b0bf07bff16857233f9cc8c953f86998abe
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-08-22 18:26:35 +0600

    all calculation done

--- js/budget.js
+++ js/budget.js
@@ -2,4 +2,8 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
     document.getElementById('player-expenses').innerText = getInputFieldValueById('budget-per-player') * getTotalNumberOfPlayers();
 });
 
-document.getElementById('btn-')
+document.getElementById('btn-calculate-total').addEventListener('click', function(){
+    const totalBudgetElement = document.getElementById('total-budget');
+    
+    totalBudgetElement.innerText = getElementValueById('player-expenses') + getInputFieldValueById('manager-budget') + getInputFieldValueById('coach-budget');
+});
