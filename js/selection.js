17f53cb367e0aefd7c050776881eb45c38052392
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-08-23 00:24:13 +0600

    updated retrieving method for playername

--- js/selection.js
+++ js/selection.js
@@ -4,13 +4,14 @@ for (const button of btnSelectButtons) {
     button.addEventListener('click', function (event) {
 
         const numberOfPlayesSelected = getTotalNumberOfPlayers();
+        // console.log(event.target.parentNode.parentNode.querySelector('.card-body').querySelector('.name').innerText);
 
         if (numberOfPlayesSelected < 5) {
             const selectedPlayer = event.target;
             const selectedPlayers = document.getElementById('selected-players');
 
             // retrieving selected player's name.
-            const PlayerName = event.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
+            const PlayerName = event.target.parentNode.parentNode.querySelector('.card-body').querySelector('.name').innerText;
 
             // Add the player in the selected list.
             selectedPlayers.appendChild(createNewElement('li', PlayerName)).classList.add('mb-3');
@@ -18,7 +19,7 @@ for (const button of btnSelectButtons) {
             //Disable the button after selecting also change background
             selectedPlayer.classList.replace('btn-primary', 'btn-secondary');
             selectedPlayer.setAttribute('disabled', true);
-            
+
         }
         else {
             alert("You cannot pick more than five players!");
